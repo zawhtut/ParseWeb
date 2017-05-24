776abc99de845dd2f8106b3caa8aa2fa8733b913
Zaw Htut <zaw.sfo@gmail.com>
2017-05-23 20:47:07 -0700

    first commit

---
+++ ParseWeb/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace ParseWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("http://www.cbbank.com.mm/exchange_rate.aspx");
+
+            HtmlNode[] nodes = document.DocumentNode.SelectNodes("//td").ToArray(); //table[@id='tablepress-1']
+            foreach (HtmlNode item in nodes)
+            {
+                Console.WriteLine(item.InnerText + " ");
+            }
+            Console.Read();
+        }
+    }
+}
